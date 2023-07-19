50048d714d1c8b213b4cca259947fad15d3dc0f9
kiiichi <kichi3744@outlook.com>
2023-07-19 21:06:05 +0800

    Update make_project.tcl

--- prj/Examples/make_project.tcl
+++ prj/Examples/make_project.tcl
@@ -1,7 +1,7 @@
 # Uncomment the example you want to run
 
-set project_name "Led_blink"
-#set project_name "Knight_rider"
+#set project_name "Led_blink"
+set project_name "Knight_rider"
 #set project_name "Stopwatch"
 #set project_name "Frequency_counter"
 #set project_name "Simple_moving_average"
